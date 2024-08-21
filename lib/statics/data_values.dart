f6c710908cbe8ff2b357ead728d766361b09b160
Mateus Mourao <mateusvmourao@gmail.com>
2024-08-21 19:52:07 -0300

    feat: adicionando temas e infos estáticas

---
+++ lib/statics/data_values.dart
@@ -0,0 +1,95 @@
+class DataValues {
+  // About App
+  static const String appName = "Mateus Mourão";
+  static const String appNameLong = "mathieux.dev";
+  static const String appDescription =
+      "Bem-vindo ao meu portfólio!\nAqui, você encontrará uma vitrine dos meus projetos, habilidades e empreendimentos criativos. Vamos nos conectar e colaborar!";
+  static const String appDeveloper = "Mateus Mourão";
+  static const String appPackage = "dev.mathieux.mathieux_dev";
+  static const String appVersion = "1.0.0";
+  static const String appBuildNumber = "1";
+
+  // External links
+  static Uri githubURL = Uri.parse("https://github.com/mathieux-dev");
+  static Uri linkedinURL =
+      Uri.parse("https://www.linkedin.com/in/mateus-mourao");
+  static Uri instagramURL = Uri.parse("https://instagram.com/eimathieu");
+  static Uri repoURL =
+      Uri.parse("https://github.com/mathieux-dev/mathieux-dev");
+
+  // Header
+  static const String headerGreetings = "Olá! Eu sou";
+  static const String headerName = "Mateus Mourão";
+  static const String headerTitle =
+      "Estudante | Desenvolvedor | Parceiro estratégico";
+
+  // Navigation Bar
+  static const String navBarAboutMe = "Sobre Mim";
+  static const String navBarEducation = "Educação";
+  static const String navBarExperience = "Experiência";
+  static const String navBarContactMe = "Contato";
+
+  // About me
+  static const String aboutPortfolio =
+      "Bem-vindo ao meu portfólio!\nAqui, você encontrará uma vitrine dos meus projetos, habilidades e experiências profissionais. Vamos nos conectar e colaborar!.";
+  static const String aboutMeTitle = "Sobre Mim";
+  static const String aboutMeDescription =
+      "Olá! Sou Mateus Mourão, desenvolvedor de software e entusiasta por tecnologia. Acredito que o crescimento pessoal e a busca por novos conhecimentos são essenciais para o sucesso."
+      "\nPor isso, estou sempre aprendendo e aplicando novas habilidades em meus projetos. Minha jornada profissional inclui experiências significativas do ramo financeiro, onde atuei em projetos desafiadores e de grande impacto.";
+
+  static const String aboutMeStudentTitle = "Estudante de Tecnologia";
+  static const String aboutMeStudentDescription =
+      "Sou estudante de tecnologia com uma sólida formação em engenharia de software, iniciada na UFC, onde cursei Sistemas de Informação e mesmo não concluindo, formei uma base sólida de conhecimentos.\nAtualmente, estou cursando Análise e Desenvolvimento de Sistemas e além dos estudos acadêmicos, dedico-me ao aprendizado pessoal na área de software, focando em linguagens de programação, arquitetura de sistemas e padrões de projeto.";
+  static const String aboutMeDeveloperTitle = "Desenvolvedor de Soluções";
+  static const String aboutMeDeveloperDescription =
+      "Sou desenvolvedor de software com uma ampla gama de habilidades, focado em criar soluções eficientes e resolver problemas complexos. Minha experiência inclui desenvolvimento backend e frontend, onde estou familiarizado com diversas tecnologias.\nTenho um sólido entendimento de arquitetura de software, microsserviços e padrões de projeto, além de aplicar metodologias ágeis como Scrum para garantir entregas de alta qualidade.";
+  static const String aboutMeTargetTitle = "Objetivos";
+  static const String aboutMeTargetDescription =
+      "Meu objetivo como desenvolvedor de software é resolver problemas de maneira eficaz, apresentar soluções inovadoras e estar sempre disponível para novos desafios. Tenho um forte compromisso em identificar e entender as necessidades dos projetos, aplicando meu conhecimento em diversas tecnologias para desenvolver soluções que agreguem valor. Estou constantemente buscando aprender e crescer, sempre pronto para enfrentar e superar desafios.";
+
+  // Education
+  static const String educationTitle = "Educação";
+  static const String educationDescription =
+      "Minha formação educacional é diversificada e vem de várias fontes. Nesta seção, você pode descobrir sobre minha educação formal e outras fontes de aprendizado. Aqui estão meus cursos de graduação e outras plataformas de aprendizado nas quais estive envolvido.";
+  static const String educationOrg1Title =
+      "Universidade Federal do Ceará - Campus Crateús";
+  static const String educationOrg1CourseName = "Sistemas de Informação";
+  static const String educationOrg1CourseType = "Bacharelado";
+  static const String educationOrg1CoursePeriod = "2018 - 2021 (interrompido)";
+
+  static const String educationOrg2Title = "FAM - Faculdade das Américas";
+  static const String educationOrg2CourseName =
+      "Análise e Desenvolvimento de Sistemas";
+  static const String educationOrg2CourseType = "Superior de Tecnologia";
+  static const String educationOrg2CoursePeriod = "2024 - Atualmente";
+
+  // Experience
+  static const String experienceTitle = "Experiência";
+  static const String experienceDescription =
+      "Minhas experiências profissionais são variadas e enriquecedoras. Nesta seção, você pode conhecer meu histórico profissional e as diferentes áreas em que atuei.\nAqui estão os papéis que desempenhei ao longo da minha carreira, onde pude desenvolver e aplicar minhas habilidades em desenvolvimento de software.";
+  static const String experienceOrg1Title = "Fundação ASTEF - UFC + Fitbank";
+  static const String experienceOrg1Role = "Desenvolvedor Backend";
+  static const String experienceOrg1Years = "Janeiro 2021 - Junho 2021";
+  static const String experienceOrg1Vales =
+      "C#, .NET, Entity Framework, PostgresSQL, Scrum, Taiga, Clockfy, Postman.\n";
+  static const String experienceOrg2Title = "Fitbank Pagamento Eletrônicos";
+  static const String experienceOrg2Role = "Desenvolvedor Fullstack";
+  static const String experienceOrg2Years = "Junho 2021 - Outubro 2021";
+  static const String experienceOrg2Vales =
+      "C#, .NET, ASP.NET, JavaScript, ReactJS, JQuery, Ajax, CSS, HTML, Entity Framework, SQL Server, PostgresSQL, Scrum, Azure, Postman\n";
+  static const String experienceOrg3Title = "Eletric Dreams";
+  static const String experienceOrg3Role = "Desenvolvedor Fullstack";
+  static const String experienceOrg3Years = "Novembro 2021 - Março 2024";
+  static const String experienceOrg3Vales =
+      "C#, .NET, ASP.NET, Entity Framework, Dapper, JavaScript, NodeJS, ReactJS, Angular, CSS, Bootstrap, HTML, SQL Server, Testes unitários, Scrum, Kanban, Azure, Postman";
+
+  // Contact
+  static const String contactTitle = "Contato";
+  static const String contactDescription =
+      "Agradeço por olhar meu perfil público. Sinta-se à vontade para me perguntar qualquer coisa ou para discutir qualquer assunto comigo.\nFico feliz em conversar e conhecer novas pessoas. Caso seja do seu interesse, me mande uma mensagem pelo LinkedIn, e-mail ou Instagram.";
+  static const String contactBanner = "Me mande um e-mail em";
+  static const String contactEmail = "mateusvmourao@gmail.com";
+
+  // Footer
+  static const String builtWith = "Feito com Flutter e GitHub Pages.";
+}
