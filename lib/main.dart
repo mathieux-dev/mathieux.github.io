3fb1c307129380339a2d944fc525f52b32263f34
Mateus Mourao <mateusvmourao@gmail.com>
2024-08-22 01:35:55 -0300

    feat: material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:portifolio/home-page/desktop/about-me.desktop.dart';
+import 'package:portifolio/home-page/desktop/contact.desktop.dart';
+import 'package:portifolio/home-page/desktop/education.desktop.dart';
+import 'package:portifolio/home-page/desktop/experience.desktop.dart';
+import 'package:portifolio/home-page/desktop/footer.desktop.dart';
+import 'package:portifolio/home-page/desktop/header.desktop.dart';
+import 'package:portifolio/home-page/mobile/about-me.mobile.dart';
+import 'package:portifolio/home-page/mobile/contact.mobile.dart';
+import 'package:portifolio/home-page/mobile/education.mobile.dart';
+import 'package:portifolio/home-page/mobile/experience.mobile.dart';
+import 'package:portifolio/home-page/mobile/foorter.mobile.dart';
+import 'package:portifolio/home-page/mobile/header.mobile.dart';
+import 'package:portifolio/themes/app_theme.dart';
+import 'package:portifolio/themes/responsive_provider.dart';
+import 'package:portifolio/widgets/bars/navigation-bar.widget.dart' as app_bar;
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +22,119 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Mateus Mourão',
+      debugShowCheckedModeBanner: false,
+      theme: AppThemeData.darkTheme,
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  bool _showBackToTopButton = false;
+  late ScrollController _scrollController;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    _scrollController = ScrollController()
+      ..addListener(
+        () {
+          setState(
+            () {
+              if (_scrollController.offset >= 300) {
+                _showBackToTopButton = true;
+              } else {
+                _showBackToTopButton = false;
+              }
+            },
+          );
+        },
+      );
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollToTop() {
+    _scrollController.animateTo(0,
+        duration: const Duration(milliseconds: 1000), curve: Curves.easeInOut);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    Widget desktopUI() {
+      return ListView(
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        children: const [
+          DesktopHeader(),
+          DesktopAboutMe(),
+          DesktopEducation(),
+          DesktopExperience(),
+          DesktopContact(),
+          DesktopFooter(),
+        ],
+      );
+    }
+
+    Widget mobileUI() {
+      return ListView(
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        children: const [
+          MobileHeader(),
+          MobileAboutMe(),
+          MobileEducation(),
+          MobileExperience(),
+          MobileContact(),
+          MobileFooter(),
+        ],
+      );
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      appBar: ResponsiveScreenProvider.isDesktopScreen(context)
+          ? null
+          : AppBar(elevation: 0.0),
+      drawer: ResponsiveScreenProvider.isDesktopScreen(context)
+          ? null
+          : app_bar.NavigationBar().mobileNavBar(),
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        physics: const BouncingScrollPhysics(),
+        child: Container(
+          color: AppThemeData.backgroundGrey,
+          child: ResponsiveScreenProvider.isDesktopScreen(context)
+              ? desktopUI()
+              : mobileUI(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: _showBackToTopButton == false
+          ? null
+          : FloatingActionButton(
+              onPressed: _scrollToTop,
+              tooltip: 'Go to top',
+              backgroundColor: AppThemeData.buttonPrimary,
+              foregroundColor: AppThemeData.iconSecondary,
+              child: const Icon(
+                Icons.arrow_upward_rounded,
+              ),
+            ),
     );
   }
 }
